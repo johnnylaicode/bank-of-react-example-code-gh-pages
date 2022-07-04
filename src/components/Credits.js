a749590b42b48cd753c24af948d650fa47a774a3
MichaelTessey1 <michaeltessey1@gmail.com>
2022-07-03 20:22:06 -0400

    display credits work

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -3,10 +3,19 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 const Credits = (props) => {
-  console.log("credit ", props)
+  let displayCredits = () =>{
+    const {credits} = props;
+    return credits.map((credit) => {
+        let date = credit.date.slice(0,10);
+        return <li key = {credit.id}> {credit.amount} {credit.description} {date}</li>
+    })
+  }
+
   return (
     <div>
       <h1>Credits</h1>
+      
+      {displayCredits()}
 
       <Link to="/">Return to Home</Link>
     </div>
